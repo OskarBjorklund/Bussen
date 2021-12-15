2d4905d0b4155c2157fca4521a0c9969036b18f1
OskarBjorklund <92572944+OskarBjorklund@users.noreply.github.com>
2021-12-15 14:42:31 +0100

    Nästan klar, viss felhantering behövs och buggar.

--- Prototyp/bussprototyp.py
+++ Prototyp/bussprototyp.py
@@ -13,6 +13,7 @@ lagrar passagerare i en lista.
 import random as rand
 import json
 import os
+from os import system
 # ------------------------------ Listor --------------------------------------- #
 
 path = os.path.abspath(os.path.dirname(__file__))
@@ -69,6 +70,9 @@ class Person():
 # ------------------------- Funktionsdefinitioner ---------------------------- #
 
 # Lägger till en ny person i bussen.
+def clear_screen():
+    system("cls || clear")
+
 def plockaUpp(antal_passagerare):
     for i in range(antal_passagerare):
         if rand.random() >= 0.5:
@@ -117,6 +121,8 @@ def busSort():
 
     sorteringsVal = ""
 
+    clear_screen()
+
     while sorteringsVal != "q":
 
         print(
@@ -133,23 +139,42 @@ def busSort():
 
         if sorteringsVal == "1":
             passagerare.sort(key=lambda x: x.namn)
+            clear_screen()
             print("Bussen är nu sorterad efter förnamn")
         elif sorteringsVal == "2":
             passagerare.sort(key=lambda x: x.efternamn)
+            clear_screen()
             print("Bussen är nu sorterad efter efternamn")
         elif sorteringsVal == "3":
             passagerare.sort(key=lambda x: x.ålder)
+            clear_screen()
             print("Bussen är nu sorterad efter åldern")
         elif sorteringsVal == "4":
             passagerare.sort(key=lambda x: x.kön)
+            clear_screen()
             print("Bussen är nu sorterad efter kön")
         elif sorteringsVal == "5":
             passagerare.reverse()
+            clear_screen()
             print("Passagerare har nu blivit omvända")
+        else:
+            clear_screen()
+            print("Felaktig inmatning.")
 
 # Skriver ut en lista på alla passagerare inom ett visst åldersspann.
-def hittaPassagerare(åldersSpann):
-    return
+def hittaPassagerare(åldersSpann1, åldersSpann2):
+    ålderSpann_personer = []
+    for person in passagerare:
+        if person.ålder >= åldersSpann1 and person.ålder <= åldersSpann2:
+            ålderSpann_personer.append(person)
+    
+    print("")
+
+    if len(ålderSpann_personer) == 0:
+        print(f"Det finns inga passagerare mellan {åldersSpann1} år och {åldersSpann2} år.")
+    else:
+        for i, person in enumerate(ålderSpann_personer):
+            print(f"{i+1}. {person}")
 
 # petar på en passagerare. Skriver ut en text som beskriver passagerarens
 # reaktion när denne blir petad på. För lite svårare uppgift kan reaktionerna
@@ -166,43 +191,54 @@ def peta(petad):
             print("HJÄLP!!! Jag blir antastad av busschauffören!!!")
     elif petad_i_funktion.ålder <= 18:
         if petad_i_funktion.kön == "Han":
-            print("Yelaen Tarekhek") # Damn your entire history
+            print("Yelaen Tarekhek!!")  # Damn your entire history
         else:
             print("Din knullbulle, vad håller du på med!")
     elif petad_i_funktion.ålder <= 26:
         if petad_i_funktion.kön == "Han":
-            print("Kille reaktion")
+            print("Vad tror du att du håller på med?")
         else:
             print("Jag ringer polisen!!!")
-    elif petad_i_funktion.ålder <= :
+    elif petad_i_funktion.ålder <= 30:
         if petad_i_funktion.kön == "Han":
-            print("Kille reaktion")
+            print("Papi vad gör du?")
         else:
-            print("Tjej reaktion")
-    elif petad_i_funktion.ålder <= ÅLDER:
+            print("Bort med tassarna ditt äckel")
+    elif petad_i_funktion.ålder <= 55:
         if petad_i_funktion.kön == "Han":
-            print("Kille reaktion")
+            print("Sluta peta på mig din idiot!")
         else:
-            print("Tjej reaktion")
+            print("Vad gör du din gubbstrut?")
     elif petad_i_funktion.ålder <= 80:
         if petad_i_funktion.kön == "Han":
-            print("Ojj, hej ditt lilla kex, du kanske vill hjälpa mig hem?")
+            print("Ojj, hej ditt lilla kex, du kanske vill hjälpa mig hem?") 
         else:
             print("Vad håller du på med?")
+    elif petad_i_funktion.ålder <= 90:
+        if petad_i_funktion.kön == "Han":
+            print("Jag kanske har Alzheimers men jag har åtminstone inte Alzheimers")
+        else:
+            print("Har du problem med gammla män!")
     elif petad_i_funktion.ålder <= 110:
         if petad_i_funktion.kön == "Han":
-            print("Nämen vad i hela friden tror du att du håller på med unga man?")
+            print("Nämen du din jävel, nu ska du lyssna, fasiken vad du ska lyssna. Så gör man inte här i denna foster land. Nu ska du fannnn få käft i dig jävla ouppfostrad... Fy fan, tappade andan.")
         else:
             print(f"Snälla låta en gammal dam vara, jag är faktiskt {petad_i_funktion.ålder} år.")
-        
-    
-
 # ------------------------------ Huvudprogram --------------------------------- #
 def main():
     menyVal = ""
 
+    clear_screen()
+
     while menyVal != "q":
 
+        print(
+        """
+                                           _____________
+                                         _/_|[][][][][] | - -
+                                        (      Bussen   | - -
+                                        =--OO-------OO--=
+        """)
         print(
         """
                                          --- MENY ---
@@ -220,61 +256,141 @@ def main():
         if menyVal == "1":
             antal = rand.randint(0, 10)
             if len(passagerare) == 25:
+                clear_screen()
                 print("Bussen är för närvarande full.")
             elif antal + len(passagerare) > 24:
                 for i in range(antal):
                     plockaUpp(1)
                     if len(passagerare) == 25:
+                        clear_screen()
                         print(f"Det fanns {antal} passagerare på busshålsplatsen men endast {i} kunde stiga på.")
                         break
                     else:
                         continue
             else:
                 plockaUpp(antal)
+                clear_screen()
                 print(f"{antal} ny(a) passagerare steg ombord bussen.")
         elif menyVal == "2":
             if len(passagerare) > 10:
                 antal = rand.randint(0, 10)
                 gåAv(antal)
+                clear_screen()
                 print(f"{antal} passagerare steg av bussen.")
             elif len(passagerare) >= 1:
                 antal = rand.randint(0, len(passagerare))
                 gåAv(antal)
+                clear_screen()
                 print(f"{antal} passagerare steg av bussen.")
             else:
+                clear_screen()
                 print("Inga passagerare befinner sig på bussen.")
         elif menyVal == "3":
             if len(passagerare)>= 1:
+                clear_screen()
                 skrivUt()
             else:
+                clear_screen()
                 print("Inga passagerare befinner sig på bussen.")
         elif menyVal == "4":
+            clear_screen()
             print(f" Den sammanlagda åldern hos de {len(passagerare)} passagerare är {sammanlagdÅlder()}.")
         elif menyVal == "5":
+            clear_screen()
             print(f" Medelåldern hos de {len(passagerare)} passagerare är {medelÅlder()}.")
         elif menyVal == "6":
             if len(passagerare)>= 1:
+                clear_screen()
                 äldst()
             else:
+                clear_screen()
                 print("Inga passagerare befinner sig på bussen.")
         elif menyVal == "7":
+            clear_screen()
             busSort()
         elif menyVal == "8":
-            pass
+            if len(passagerare)>= 1:
+                while True:
+                    åldersSpann1 = input("Ange lägsta åldern på passagerare du vill hitta -> ")
+                    if åldersSpann1.isdigit():
+                        while True:
+                            åldersSpann2 = input("Ange högsta åldern på passagerare du vill hitta -> ")
+                            if åldersSpann2.isdigit():
+                                clear_screen()
+                                hittaPassagerare(int(åldersSpann1),int(åldersSpann2))
+                                input(
+                                """
+
+                                Tryck på valfri knapp för att fortsätta...
+                                """)
+                                main()
+                            else:
+                                clear_screen()
+                                print("Felaktig inmatning.")
+                                continue
+                    else:
+                        clear_screen()
+                        print("Felaktig inmatning.")
+                        continue
+            else:
+                clear_screen()
+                print("Inga passagerare befinner sig på bussen.")
         elif menyVal == "9":
             if len(passagerare)>= 1:
-                skrivUt()
-                petad_passagerare = int(input("Välj en passagerare att peta på. -> "))
-                peta(petad_passagerare)
+                clear_screen()
+                while True:
+
+                    skrivUt()
+
+                    petad_passagerare = input(
+                    """
+
+                    Välj en passagerare att peta på. -> 
+
+                    """)
+
+                    if petad_passagerare.isdigit():
+                        if int(petad_passagerare) != 0:
+                            if int(petad_passagerare) <= len(passagerare)-1:
+                                clear_screen()
+                                peta(int(petad_passagerare))
+                                input(
+                                """
+
+                                Tryck på valfri knapp för att fortsätta...
+                                """)
+                                main()
+                            else:
+                                clear_screen()
+                                print("Felaktig inmatning.")
+                                input(
+                                """
+
+                                Tryck på valfri knapp för att fortsätta...
+                                """)
+                                continue
+                        else: 
+                            clear_screen()
+                            print("Felaktig inmatning.")
+                            input(
+                            """
+
+                            Tryck på valfri knapp för att fortsätta...
+                            """)
+                            continue
+                    else:
+                        clear_screen()
+                        print("Felaktig inmatning.")
+                        input(
+                        """
+
+                            Tryck på valfri knapp för att fortsätta...
+                        """)
+                        continue
             else:
+                clear_screen()
                 print("Inga passagerare befinner sig på bussen.")
-
-print(
-"""
-                                           _____________
-                                         _/_|[][][][][] | - -
-                                        (      Bussen   | - -
-                                        =--OO-------OO--=
-""")
-
+        else:
+            clear_screen()
+            print("Felaktig inmatning.")
 main()
